fa4aa3e531a71b67e4afc89ee130c9a02bcdb2f2
Grant Lucas <contact@grantlucas.com>
2020-10-17 21:12:24 -0400

    Initial setup of basic test suite

---
+++ tests/acceptance/BasicTestCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class BasicTestCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToLoadGoogle(AcceptanceTester $I)
+    {
+    }
+}
